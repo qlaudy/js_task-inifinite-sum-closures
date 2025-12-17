40c08457f84262f35fdc521bd56a5348831d6baa
unknown <dkuz6323@gmail.com>
2025-12-17 19:50:13 +0200

    completed tasks

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,28 @@
  */
 function makeInfinityAdder() {
   // write code here
+
+  let sum = 0;
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    if (args.length > 0) {
+      for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+      }
+
+      return adder;
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
